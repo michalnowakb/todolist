9f0e64626087b49908e617f695c8154b5b6f2e22
Michal Nowak <michal.nowakb@gmail.com>
2020-01-16 17:39:09 +0000

    Updated version of ToDoList app

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -21,6 +21,7 @@ export class TodoItem extends Component {
                 <p>
                     <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />  { '  ' }
                     { title }
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
                 </p>
             </div>
         )
@@ -29,8 +30,19 @@ export class TodoItem extends Component {
 
 //PropTypes
 TodoItem.propTypes = {
-    todos: PropTypes.object.isRequired
+    todos: PropTypes.object.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
 }
 
+const btnStyle = {
+    background: '#ff0000',
+    color: 'white',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
 
 export default TodoItem;
