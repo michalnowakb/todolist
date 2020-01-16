e5003c4cdd365178275b06e89c2758782cab9c75
Michael Nowak <michal.nowakb@gmail.com>
2020-01-16 02:16:07 +0000

    initialising new repository

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Meeting with Boss',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Cinema with friends',
+        completed: false
+      }
+    ]
+  }
+
+  //Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+    render(){
+  
+    return (
+      <div className="App">
+     <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
+}
+
+export default App;
